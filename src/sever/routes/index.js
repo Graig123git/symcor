8646f3d3e54dfbe69f13d92af2e87e79e2a09ea9
GraigAnthony.Mattis <graiganthony.mattis@aircanada.ca>
2021-05-03 13:10:32 -0400

    data changes

--- src/sever/routes/index.js
+++ src/sever/routes/index.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const axios = require('axios').default;
 let router = express.Router();
+const https = require('https');
 
 router.post('/create', async (req, res) => {
-	try {
+	/*try {
 		const Url = 'xxx';
 		let axiosConfig = {
 			headers: {
@@ -19,7 +20,41 @@ router.post('/create', async (req, res) => {
 		return res.send({ statusCode: 200, data: base64String });
 	} catch (error) {
 		res.send({ err: error.message, status: 500 });
-	}
+	}*/
+
+	//using request for now
+
+	const options = {
+		hostname: 'whatever.com',
+		port: 443,
+		path: '/todos',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': data.length,
+			'x-api-key': 'xx',
+			'x-request-id': 'xxx',
+			'x-fapi': 'xxx',
+			'x-app-cat-id': 'xxx',
+		},
+	};
+
+	const req = https.request(options, (res) => {
+		console.log(`statusCode: ${res.statusCode}`);
+
+		res.on('data', (d) => {
+			console.info(d);
+			res.send({ data: error, status: 200 });
+		});
+	});
+
+	req.on('error', (error) => {
+		console.error(error);
+		res.send({ err: error, status: 500 });
+	});
+
+	req.write(data);
+	req.end();
 });
 
 router.post('/getToken', async (req, res) => {
